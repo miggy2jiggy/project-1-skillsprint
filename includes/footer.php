6347d2d631b9a72eff21fd44ee2ba992349559b5
Marcus Santiago <112112284+miggy2jiggy@users.noreply.github.com>
2026-07-09 00:09:20 -0400

    Create footer.php
    manually adding footer.php in the includes/ folder.

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<?php
+/********************
+Shared Site Footer
+ ********************/
+?>
+
+<footer class="site-footer">
+    <p>&copy; <?php echo date("Y"); ?> SkillSprint. Created for CSC 4370.</p>
+</footer>
+
+</body>
+</html>
